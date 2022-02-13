43f00b1320ec7d2fefd60287f951fe275718a246
zigou23 <a1923458037@gmail.com>
2022-02-13 23:46:54 +0800

    Update index.php

--- api/user/index.php
+++ api/user/index.php
@@ -9,4 +9,11 @@ function getRemoteIPAddress() {
     return $_SERVER['REMOTE_ADDR'];
 }
 echo '{"ip"="'.getRemoteIPAddress().'",';
+function getRemoteIPAddress2() {
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) { 
+        return $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    return $_SERVER['REMOTE_ADDR'];
+}
+echo '"ip2"="'.getRemoteIPAddress2().'",';
 echo '"ua"="'.$_SERVER['HTTP_USER_AGENT'].'"}';
