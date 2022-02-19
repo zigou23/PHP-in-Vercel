05cc450750bcc3263c554dd1244a8e014fe92263
zigou23 <a1923458037@gmail.com>
2022-02-19 23:30:05 +0800

    ipv6

--- index.php
+++ index.php
@@ -7,5 +7,10 @@
 </head>
 <body>
     Hi There!You can find me from <a href="//www.qsim.top/api">this</a>.
+    <ol>
+        <li>api/bingimg/index.php?random=1&value=1-7&server=1</li>
+        <li>api/hitokoto/index.php?type=hitokoto.txt&charset=utf-8/gbk&encode=js/js2</li>
+        <li>api/user/index.php[ip.php/ipinfo.php]</li>
+    </ol>
 </body>
 </html>
