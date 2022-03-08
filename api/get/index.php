a8803455b62e28fac9cf6bd8ec7182df425d684b
zigou <40587032+zigou23@users.noreply.github.com>
2022-03-08 23:52:47 +0800

    Create index.php

---
+++ api/get/index.php
@@ -0,0 +1,13 @@
+<?php
+// & -php无法识别，需转换成 %26 无解 php会在&处分割
+  set_time_limit(1); //限制超时2s停止
+  $url = $_GET['url'];
+  $web = file_get_contents($url);
+  $file = strlen($web);
+  if(!empty($url)){ //有值，组装数据
+    // if ($file < 1500){ } //数值小，则输出
+      $result['status'] = 200;
+      $result['data'] = file_get_contents($url);
+  }else //无值，返回状态码220
+    $result['status'] = 220;
+  echo json_encode($result); //返回JSON数据
