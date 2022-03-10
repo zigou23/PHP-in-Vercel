d715181abf872f4ee5e38d2ea9f5133f8d99d091
zigou23 <a1923458037@gmail.com>
2022-03-10 15:56:32 +0800

    Update index.php

--- api/get/index.php
+++ api/get/index.php
@@ -2,7 +2,7 @@
 // & -php无法识别，需转换成 %26 无解 php会在&处分割
   set_time_limit(1); //限制超时2s停止
   error_reporting(0);
-  $url = $_GET['url'];
+  $url = 'https://'.$_GET['url'];
   $web = file_get_contents($url);
   // $file = strlen($web);
   if(@$_GET['json'] === '1'){
