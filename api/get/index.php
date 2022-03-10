2de0e7f74179a6c93dde238c33a484069b7df45b
zigou23 <a1923458037@gmail.com>
2022-03-11 00:09:46 +0800

    Update index.php

--- api/get/index.php
+++ api/get/index.php
@@ -6,17 +6,15 @@
   $url = 'https://'.$_GET['url'];
   $web = file_get_contents($url);
   // $file = strlen($web);
-  if(@$_GET['json'] === '1'){
-    if(!empty($url)){ //有值，组装数据
+  if(!empty($url)){ //有值，组装数据
+    if(@$_GET['json'] === '1'){
       // if ($file < 1500){ } //数值小，则输出
       $result['status'] = 200;
       $result['data'] = file_get_contents($url);
       echo json_encode($result); //返回JSON数据
-    }else //无值，返回状态码220
-      $result['status'] = 220;
-  }
-  else{
-    header('Content-Type: TXT');
-    echo "/* DO NOT USE IT!!! */\r\n";
+    }else{
+    header('Content-Type: text/plain;charset=UTF-8');
+    echo "/**/\n";
     echo $web;
   }
+}
