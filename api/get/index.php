cb2677a14b66a88d4bd87d6308722aa444a57a48
zigou23 <a1923458037@gmail.com>
2022-03-11 22:03:10 +0800

    change it

--- api/get/index.php
+++ api/get/index.php
@@ -1,20 +1,24 @@
 <?php
-// & -php无法识别，需转换成 %26 无解 php会在&处分割
-  // set_time_limit(1); //限制超时2s停止
-  ini_set('max_execution_time', 1); //300 seconds = 5 minutes
-  error_reporting(0);
-  $url = 'https://'.$_GET['url'];
-  $web = file_get_contents($url);
-  // $file = strlen($web);
-  if(!empty($url)){ //有值，组装数据
-    if(@$_GET['json'] === '1'){
-      // if ($file < 1500){ } //数值小，则输出
-      $result['status'] = 200;
-      $result['data'] = file_get_contents($url);
-      echo json_encode($result); //返回JSON数据
-    }else{
-    header('Content-Type: text/plain;charset=UTF-8');
-    echo "/**/\n";
-    echo $web;
+set_time_limit(1); 
+error_reporting(0); //不输出错误
+header('Content-Type: text/plain;charset=UTF-8');
+if(is_array($_GET)&&count($_GET)>0){//判断是否有Get参数
+  if(isset($_GET["file"])){//判断file的参数是否存在,isset用来检测变量是否设置,返回T&F
+    if(isset($_GET["url"])){// url=?
+      $file=$_GET["file"];
+      $url = 'https://'.$_GET['url'].'.'.$file;
+      $web = file_get_contents($url);
+      if(@$_GET['json'] === '1'){ // JSON数值时
+        $result['status'] = 200;
+        $result['data'] = $web;
+        echo json_encode($result); //返回JSON数据
+      }elseif ($file === 'png' || $file === 'webp') //if仅使用这几个类型
+        echo $web;
+      elseif ($file === 'js' || $file === 'css') {
+        echo "/* DO NOT SHARE WITH OTHERS */\n";
+        echo $web;
+      }
+    }
   }
-}
+}else
+  echo 'What would you like to do? Nothing in there!';
