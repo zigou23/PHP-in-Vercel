d907d241abe5d60b7b19fa662e815cecd49ce3be
zigou23 <a1923458037@gmail.com>
2022-03-29 23:41:38 +0800

    Update ip.php

--- api/user/ip.php
+++ api/user/ip.php
@@ -1,20 +1,13 @@
 <?php 
-function getIP() /*获取客户端IP*/ 
-{ 
-if (@$_SERVER["HTTP_X_FORWARDED_FOR"]) 
-$ip = $_SERVER["HTTP_X_FORWARDED_FOR"]; 
-else if (@$_SERVER["HTTP_CLIENT_IP"]) 
-$ip = $_SERVER["HTTP_CLIENT_IP"]; 
-else if (@$_SERVER["REMOTE_ADDR"]) 
-$ip = $_SERVER["REMOTE_ADDR"]; 
-else if (@getenv("HTTP_X_FORWARDED_FOR")) 
-$ip = getenv("HTTP_X_FORWARDED_FOR"); 
-else if (@getenv("HTTP_CLIENT_IP")) 
-$ip = getenv("HTTP_CLIENT_IP"); 
-else if (@getenv("REMOTE_ADDR")) 
-$ip = getenv("REMOTE_ADDR"); 
-else 
-$ip = "Unknown"; 
-return $ip; 
-} 
-echo getIP();
+$a = $_SERVER["HTTP_X_FORWARDED_FOR"];
+echo 'a'.$a."\n";
+$aa = getenv("HTTP_X_FORWARDED_FOR");
+echo 'aa'.$aa."\n";
+$b = $_SERVER["HTTP_CLIENT_IP"];
+echo 'b'.$b."\n";
+$bb = getenv("HTTP_CLIENT_IP");
+echo 'bb'.$bb."\n";
+$c = $_SERVER["REMOTE_ADDR"];
+echo 'c'.$c."\n";
+$cc = getenv("REMOTE_ADDR");
+echo 'cc'.$cc."\n";
